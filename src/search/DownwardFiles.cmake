1632b752f19e105f5ae90fdc9747fc36ea5573d8
Philippe Heim <s8phheim@stud.uni-saarland.de>
2021-04-27 13:39:01 +0200

    use rng, start documenting fuzzers, add seperate coverage conceps and fact coverage

--- src/search/DownwardFiles.cmake
+++ src/search/DownwardFiles.cmake
@@ -905,10 +905,10 @@ fast_downward_plugin(
         policy_fuzzing/fuzzers/initial_state_random_walk
         policy_fuzzing/fuzzers/pool_random_walk
         policy_fuzzing/fuzzers/mixed_fuzzer
-        policy_fuzzing/fuzzers/random_choices
         policy_fuzzing/oracles/oracle
         policy_fuzzing/oracles/oracles_plan_opt
         policy_fuzzing/oracles/oracles_plan_sat
+        policy_fuzzing/coverage
         policy_fuzzing/fuzzer_bugs
         policy_fuzzing/policy_fuzzing
     DEPENDS TASK_PROPERTIES POLICIES SEARCH_COMMON
